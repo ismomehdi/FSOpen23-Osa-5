26a351ccf9ceee8c182deedc1b76f0a762115fc8
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-04-12 17:13:32 +0300

    5.2: blogilistan frontend, step2

--- src/App.js
+++ src/App.js
@@ -49,6 +49,11 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <h2>log in to application</h2>
@@ -77,6 +82,8 @@ const App = () => {
   const showBlogs = () => (
     <div>
       <h2>blogs</h2>
+      {user.username} logged in
+      <button onClick={handleLogout}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
