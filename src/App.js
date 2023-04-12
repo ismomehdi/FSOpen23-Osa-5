02d537c8dc97c9947a36b5f4bd6e2245a051163f
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-04-12 17:40:04 +0300

    5.3: blogilistan frontend, step3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
+
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
   // const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
@@ -84,11 +88,64 @@ const App = () => {
       <h2>blogs</h2>
       {user.username} logged in
       <button onClick={handleLogout}>logout</button>
+      {createBlogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+
+    const newBlog =await blogService.create(blogObject)
+    setBlogs(blogs.concat(newBlog))
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
+    
+
+
+  const createBlogForm = () => (
+      <form onSubmit={handleCreateBlog}>
+        <h2>create new</h2>
+        <div>
+          title:
+          <input
+            type="text"
+            value={newTitle}
+            name="Title"
+            onChange={({ target }) => setNewTitle(target.value)}
+          />
+        </div>
+        <div>
+          author: 
+          <input
+            type="text"
+            value={newAuthor}
+            name="Author"
+            onChange={({ target }) => setNewAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url: 
+          <input
+            type="text"
+            value={newUrl}
+            name="Url"
+            onChange={({ target }) => setNewUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    )
 
 
   return (
