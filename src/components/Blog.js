5c4d683776786af53c16ab9cd6b14ed1355ee986
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-04-18 15:01:16 +0300

    5.7 blogilistan frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const buttonLabel = visible ? 'hide' : 'view'
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return  (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{buttonLabel}</button>
+      <div style={showWhenVisible}>
+        {blog.url} <br />
+        likes {blog.likes} <button>like</button> <br />
+        {blog.user.username}
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
