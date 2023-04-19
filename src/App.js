df58a43ca64bf346cf06b6f1afe00bdb6fa4b9b9
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-04-19 15:57:59 +0300

    5.10: blogilistan frontend, step10

--- src/App.js
+++ src/App.js
@@ -88,8 +88,10 @@ const App = () => {
       {user.username} logged in
       <button onClick={handleLogout}>logout</button>
       {blogForm()}
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
       )}
     </div>
   )
